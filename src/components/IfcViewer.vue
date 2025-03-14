0f6072aecd41ec62dcb330e5647747ac556935c2
Mohamed-JJ <simojarboue24@gmail.com>
2025-03-14 17:08:43 +0000

    working on exporting 3d modules as gltf

--- src/components/IfcViewer.vue
+++ src/components/IfcViewer.vue
@@ -3,12 +3,118 @@ import { ref, onMounted } from "vue";
 import * as OBC from "@thatopen/components";
 import * as BUI from "@thatopen/ui";
 import * as BUIC from "@thatopen/ui-obc";
+import { GLTFExporter } from "three/addons/exporters/GLTFExporter.js";
+import {} from "@utils/"
 
 // Refs for DOM elements
 const container = ref(null);
 const modelsList = ref(null);
 const loadIfcButton = ref(null);
+const functionRef = ref(null);
+const worldRef = ref(null);
+const worldsRef = ref(null);
+const last_modelRef = ref(null);
 // const buttonRef = BUIC.buttons.loadIfc({ components });
+
+const handleClick = () => {
+  alert("button clicked");
+};
+
+function triggerFileUpload() {
+  const fileInput = document.getElementById("ifc-file-input");
+  if (fileInput) {
+    console.log("clicked on upload element");
+    fileInput.click();
+  } else {
+    console.error("File input element not found");
+  }
+}
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+async function handleFileUpload(event) {
+  console.log("handle upload invoked");
+  const file = event?.target?.files[0];
+  if (!file) {
+    alert("No file selected!");
+    return;
+  }
+  console.log("the ifc file name is :", event.target.files[0].name);
+  try {
+    console.log("1");
+    const data = await readFile(file);
+    const buffer = new Uint8Array(data);
+    const model = await fragmentIfcLoader.load(buffer);
+    world.scene.three.add(model);
+
+    console.log("2");
+    await setupEntityAttributes(model);
+
+    metadata = model.getLocalProperties();
+    last_model = model;
+    console.log("[INIT Load] metadata : ", metadata);
+    console.log("[INIT Load] model : ", last_model);
+    console.log("3");
+  } catch (error) {
+    console.error("Error loading IFC file:", error);
+    alert("Failed to load IFC file.");
+  }
+}
+
+const exportGLTF = () => {
+  alert("export clicked");
+  const exporter = new GLTFExporter();
+  //Setup the last model's properties
+  var tmpo = [];
+  last_modelRef.value.traverse((el) => {
+    console.log("smsm", e1);
+    tmpo.push(el);
+    // var props = fragments.
+  });
+
+  console.log("[INIT Load] Traverse ,", tmpo);
+
+  var tmpb = [];
+  var tmpc = [];
+  last_modelRef.value.items.map((el) => {
+    tmpb.push(el);
+    tmpc.push(el.exportData());
+  });
+
+  console.log("[INIT Load] items ,", tmpb);
+  console.log("[INIT Load] extractData,", tmpc);
+
+  exporter.parse(
+    worldRef.value.scene.three,
+    (gltf) => {
+      // Save GLTF
+      // gltf["extras"] = metadata;
+      if (gltf instanceof ArrayBuffer) {
+        const glbBlob = new Blob([gltf], { type: "model/gltf-binary" });
+        downloadFile(glbBlob, "model.glb");
+
+        // Save Metadata
+      }
+      // const metadataBlob = new Blob([JSON.stringify(metadata, null, 2)], { type: "application/json" });
+      // downloadFile(metadataBlob, "metadata.json");
+    },
+    (err) => {
+      console.log("[EXPORT GLTF ERR]  err ", err);
+    },
+    {
+      trs: true,
+      binary: true,
+    }
+  );
+};
+
 
 // Initialize the 3D scene and UI
 onMounted(async () => {
@@ -69,6 +175,9 @@ onMounted(async () => {
   // Append the models list to the DOM
   modelsList.value.appendChild(modelsListElement);
   loadIfcButton.value.appendChild(loadIfcBtn);
+
+  worldRef.value = world;
+  worldsRef.value = worlds;
 });
 
 // Function to load an IFC file
@@ -87,14 +196,28 @@ const loadIfc = async () => {
         label="IFC Models"
         class="absolute top-0 left-0 h-full w-[20%]"
       >
-        <bim-panel-section label="Importing" ref="loadIfcButton">
-          <!-- {{ loadIfcButton }} -->
+        <!-- <bim-panel-section label="Importing" ref="loadIfcButton">
+        </bim-panel-section> -->
+        <bim-panel-section label="Importing">
+          <bim-button label="Load IFC" @click="triggerFileUpload">
+            <input
+              type="file"
+              id="ifc-file-input"
+              accept=".ifc"
+              style="display: none"
+              @change="handleFileUpload"
+            />
+          </bim-button>
         </bim-panel-section>
+        <!-- {{ loadIfcButton }} -->
         <bim-panel-section icon="mage:box-3d-fill" label="Loaded Models">
           <div v-if="modelsList" ref="modelsList">
             <!-- {{ modelsList }} -->
           </div>
           <div v-else ref="modelsList"></div>
+        </bim-panel-section>
+        <bim-panel-section>
+          <bim-button label="Export GLTF" @click="exportGLTF"> </bim-button>
         </bim-panel-section>
       </bim-panel>
     </div>
