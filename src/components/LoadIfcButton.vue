3388f61e2502084ea52c98adc14b753442b6228a
Mohamed Jarboua <mjarboua@e1r10p13.1337.ma>
2025-03-17 13:36:27 +0000

    working on the full implementation of the services listed in the demo badr assigned to me

--- src/components/LoadIfcButton.vue
+++ src/components/LoadIfcButton.vue
@@ -3,25 +3,16 @@ import { downloadFile, readFile, triggerFileUpload } from "../utils/utils";
 </script>
 
 <template>
-    <NButton text-color="white" color="#2e3338" v-on:click="triggerFileUpload" 
-      >Load IFC
-      <input
-        type="file"
-        id="ifc-file-input"
-        accept=".ifc"
-        style="display: none"
-        @change="handleFileUpload"
-      />
-    </NButton>
-    <!-- <bim-button label="Load IFC" @click="triggerFileUpload">
-            <input
-              type="file"
-              id="ifc-file-input"
-              accept=".ifc"
-              style="display: none"
-              @change="handleFileUpload"
-            />
-          </bim-button>-->
+  <NButton text-color="white" color="#2e3338" v-on:click="triggerFileUpload">
+    Load IFC
+    <input
+      type="file"
+      id="ifc-file-input"
+      accept=".ifc"
+      style="display: none"
+      @change="handleFileUpload"
+    />
+  </NButton>
 </template>
 
 <style scoped></style>
